dea9ae2ea1d8edd93314874e529369b8a24f0b96
Groshie <36419553+Groshie@users.noreply.github.com>
2022-11-03 16:16:46 +0100

    Dev-version
    Changelog:
    - Changed number of rows read to 35
    - Fixed bugs with adding/removing vaults
    - Clarified help text

--- Dynes_Vault_Memorization.js
+++ Dynes_Vault_Memorization.js
@@ -14,41 +14,53 @@
 // Add a vault (vaultsAdd <stored name>)
 function vaultAdd(name, line, wildcards) {
     var vaultName = line.replace("vaultsAdd ", "");
-    var vaultText = world.GetRecentLines(50);
+    var vaultText = world.GetRecentLines(35);
     var currentTime = new Date();
     var exists = 0;
 
-    vaultText = vaultText.match(/(The .+?(contains|full|empty).+with.+[.])|(It is.+(full|empty) with: .+[.])|(^The.+?(contains|full|empty).+except.+[.])$/g);
+    vaultText = vaultText.match(/(The .+?(contains|full|empty).+with.+[.])|(The \w+ contains.+[.])|(It is.+(full|empty) with: .+[.])|(^The.+?(contains|full|empty).+except.+[.])$/g);
     
     var vaultList = world.getVariable("vaults");
+    var vaultObj = {};
     var newVaultList = "";
+    var exists = 0;
 
     if (vaultList == "") {
-        newVaultList = vaultName + ";" + vaultText + ";" + currentTime + "\r\n";
-        world.note("Vault " + vaultName + " added!");
+        newVaultList = vaultName + ";" + vaultText + ";" + currentTime;
+        world.note("Vault '" + vaultName + "' added!");
     } else {
         vaultList = vaultList.split("\r\n");
 
-        for (line in vaultList) {
-            var thisLine = vaultList[line].split(";");
-
-            if (thisLine[0] == vaultName) {
-                newVaultList += vaultName + ";" + vaultText + ";" + currentTime + "\r\n";
-                world.note("Vault " + vaultName + " replaced!");
-                exists = 1;
+        for (row in vaultList) {
+            if (row == "" || row == "\r\n") {
                 continue
             }
 
-            newVaultList += thisLine;
+            var thisRow = vaultList[row].split(";");
+            vaultObj[thisRow[0]] = [thisRow[1], thisRow[2]];
+
+            if (thisRow[0] == vaultName) {
+                exists = 1;
+            }
         }
 
+        vaultObj[vaultName] = [vaultText, currentTime];
+
+        for (vault in vaultObj) {
+            newVaultList += vault + ";" + vaultObj[vault][0] + ";" + vaultObj[vault][1] + "\r\n";
+        }
+
+        newVaultList = newVaultList.substr(0, newVaultList.lastIndexOf("\r\n"));
+
         if (exists == 0) {
-            newVaultList += vaultName + ";" + vaultText + ";" + currentTime + "\r\n";
-            world.note("Vault " + vaultName + " added!");
+            world.note("Vault '" + vaultName + "' added!");
+        } else {
+            world.note("Vault '" + vaultName + "' replaced!");
         }
     }
 
     world.SetVariable("vaults", newVaultList);
+    world.SaveState;
     return
 }
 
@@ -58,14 +70,17 @@ function vaultList() {
 
     if (vaultsList == "") {
         world.note("No vaults recorded yet!");
+        world.note("Use the 'vaultsAdd' command to add a vault!");
     } else {
+        world.note("--- List of recorded vaults ---");
+        world.note("");
         vaultsList = vaultsList.split("\r\n");
 
         for (line in vaultsList) {
             var thisLine = vaultsList[line].split(";");
-            world.note(thisLine[0]);
+            world.note(thisLine[0] + ", updated: " + thisLine[2]);
         }
-        world.note("--- All recorded vaults listed ---");
+        world.note("\r\n--- All recorded vaults listed ---");
     }
     return
 }
@@ -96,32 +111,48 @@ function vaultRemove(name, line, wildcards) {
     var vaultName = line.replace("vaultsRemove ", "");
     var vaultList = world.getVariable("vaults");
     var newVaultList = "";
+    var exists = 0;
     vaultList = vaultList.split("\r\n");
 
-    for (line in vaultList) {
-        var thisLine = vaultList[line].split(";");
+    for (row in vaultList) {
+        var thisLine = vaultList[row].split(";");
 
         if (thisLine[0] == vaultName) {
-            world.note("Vault with name " + thisLine[0] + " was removed!")
-            continue
+            exists = 1;
+        } else {
+            newVaultList += thisLine[0] + ";" + thisLine[1] + ";" + thisLine[2] + "\r\n";
         }
+    }
 
-        newVaultList += thisLine;
+    newVaultList = newVaultList.substr(0, newVaultList.lastIndexOf("\r\n"));
+
+    if (exists == 0) {
+        world.note("No such vault found! (Case-sensitive)");
+    } else {
+        world.note("Vault with name '" + thisLine[0] + "' was removed!");
     }
 
     world.SetVariable("vaults", newVaultList);
+    world.SaveState;
     return
 }
 
 // Helpfile
 function vaultHelp() {
+    world.note("");
+    world.note("--- HELP FOR VAULT MEMORIZATION ---");
+    world.note("");
     world.note("To add a vault, first look at your chosen container, then use the 'vaultsAdd'-command!\r\n")
+    world.note("");
     world.note("The following (case-sensitive) commands are available:");
-    world.note("'vaultsAdd': Adds a vault to the list (auto-overwrites).");
-    world.note("'vaults': Shows your current list of vaults.");
+    world.note("'vaultsAdd': Adds a vault to the list (auto-overwrites if an entry already exists).");
+    world.note("'vaults': Shows your current list of vaults and when they were last updated.");
     world.note("'vaultsShow <vault>': Shows chosen vault contents if in list.");
-    world.note("'vaultsRemove <vault>': Removes chosen vault if in list.");
+    world.note("'vaultsRemove <vault>': Removes chosen vault if it has been added to your list.");
     world.note("'vaultsHelp': Shows this information.");
+    world.note("");
+    world.note("-- HELP END ---");
+    return
 }
 
 
